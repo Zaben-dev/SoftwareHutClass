124bac37b2c8e86ed7104f44658045c0913962f2
barttgolo <bartlomiej.golon@student.uwm.edu.pl>
2021-04-20 21:01:22 +0200

    add counter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,63 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+import Button from "./button";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = (): void => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = (): void => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = (): void => {
+    setCounter(0);
+  };
+
+  const counterView = () => {
+    if (counter < 15) {
+      if (counter > 10) {
+        return (
+          <div>
+            <p>{counter}</p>
+            <p>Liczba większa od 10</p>
+          </div>
+        );
+      } else if (counter < 10) {
+        return (
+          <div>
+            <p>{counter}</p>
+            <p>Liczba mniejsza od 10</p>
+          </div>
+        );
+      } else if (counter === 10) {
+        return (
+          <div>
+            <p>{counter}</p>
+            <p>Liczba równa 10</p>
+          </div>
+        );
+      }
+    } else if (counter >= 15) {
+      return <p>Liczba większa od 15. Zresetuj liczbę.</p>;
+    }
+  };
+
+  // większy od 10 mniejszy od 10
+  // gdy większa od 15 chować counter i że przekroczył.
+
+  return (
+    <div className="App">
+      <Button name={"+"} color={"primary"} onClick={handleAdd} />
+      {counterView()}
+      <Button name={"-"} color={"secondary"} onClick={handleDecrement} />
+      <Button name={"Reset"} color={"primary"} onClick={handleReset} />
+    </div>
+  );
+};
 
 export default App;
