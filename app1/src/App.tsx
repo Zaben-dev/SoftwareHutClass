4a1307eccf582cd77e8b488aa8c1818a3ae5b19b
barttgolo <bartlomiej.golon@student.uwm.edu.pl>
2021-05-17 18:17:17 +0200

    add note 5

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import {Switch, Route, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import Home from './views/home/Home';
-import Movie from './views/movie/Movie';
-import Page404 from './views/page404/Page404';
+import Home from "./views/home/Home";
+import Movie from "./views/movie/Movie";
+import Page404 from "./views/page404/Page404";
+import store from "./store";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-
   return (
     <div className="App">
       <BrowserRouter>
-      <Switch>
-        <Route path="/movie/:id" component={Movie} />
-        <Route path="/" component={Home} exact />
-        <Route path="*" component={Page404} />
-        <Route/>
-      </Switch>
+        <Provider store={store}>
+          <Switch>
+            <Route path="/movie/:id" component={Movie} />
+            <Route path="/" component={Home} exact />
+            <Route path="*" component={Page404} />
+            <Route />
+          </Switch>
+        </Provider>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
